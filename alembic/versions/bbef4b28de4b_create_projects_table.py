509c5dfa28c0a5628b42fbf51fdc641e545f28df
Martin Levesque <levesque.martin@gmail.com>
2022-12-25 11:04:48 -0500

    status checking db

---
+++ alembic/versions/bbef4b28de4b_create_projects_table.py
@@ -0,0 +1,29 @@
+"""create projects table
+
+Revision ID: bbef4b28de4b
+Revises: 
+Create Date: 2022-12-25 10:24:03.103563
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bbef4b28de4b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'projects',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(50), nullable=False),
+        sa.Column('description', sa.String(200), nullable=True),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('projects')
